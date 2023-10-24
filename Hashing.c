e3dd3b82ce53aff3fd8ea6030be897728fff93b0
JhosepLS <143119700+JhosepLS@users.noreply.github.com>
2023-10-23 21:21:28 -0500

    Creamos el nuevo nodo con el valor proporcionado

--- Hashing.c
+++ Hashing.c
@@ -14,3 +14,10 @@ typedef struct HashTable {
     Node* bucket[BUCKET_SIZE];
     int (*hashFunc)(int);
 } HashTable;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
